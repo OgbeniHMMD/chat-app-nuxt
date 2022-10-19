637a01e96b77a2db4f7f2381c6ed9426608e672a
Hammed A. Olajide <olajide.a.hammed@gmail.com>
2022-10-19 11:22:36 +0100

    Refactoring

--- components/ChatCard.vue
+++ components/ChatCard.vue
@@ -1,10 +1,26 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="bg-white border rounded-lg bg-blue-200 p-8 w-2/3">XXX</div>
+  <div class="flex flex-col w-full">
+    <div v-if="isMe(chat.id)" class="bg-white rounded-lg bg-green-100 p-4 w-2/3 self-end">
+      {{ chat.text }}
+    </div>
+
+    <div v-else class="bg-white rounded-lg bg-green-50 p-4 w-2/3">
+      {{ chat.text }}
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "ChatCard",
-};
+<script setup lang="ts">
+defineProps<{
+  chat: {
+    id: Number,
+    text: String,
+    time: String
+  }
+}>()
+
+const ID = 419 // hardcoded ID
+const isMe = (id: Number) => id === ID
+
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,14 +10,25 @@
         </p>
       </div>
 
-      <div class="bg-white flex flex-col flex-grow p-4 gap-4 overflow-y-auto">
-        <ChatCard v-for="i of count" />
+      <div class="bg-white flex flex-col flex-grow py-8 px-4 gap-4 overflow-y-auto">
+        <div v-if="!chats.length" class="flex h-full text-center text-gray-700 justify-center items-center">
+          Nothing to show! <br><br>Be the first to send your message.
+        </div>
+
+        <div v-for="(chat,i) of chats" :key="i">
+          <ChatCard :chat="chat" />
+        </div>
       </div>
 
       <div class="bg-white border-t flex font-medium font-mono border-green-500 text-lg text-center p-2 gap-2">
-        <input placeholder="Enter Message" class="bg-transparent w-full py-2 px-4 focus:outline-0">
+        <input
+          v-model.trim="message"
+          placeholder="Enter Message"
+          class="bg-transparent w-full py-2 px-4 focus:outline-0"
+          @keypress.enter="sendChat"
+        >
 
-        <button class="bg-transparent text-green-700 hover:text-green-800" @click="increment">
+        <button class="bg-transparent text-green-700 hover:text-green-800" @click="sendChat">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -37,16 +48,34 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
+// constants
+const id = 419 // hardcoded ID
+
 // reactive state
-const count = ref(0)
+const chats = ref([])
+const message = ref('')
 
 // functions that mutate state and trigger updates
-function increment () {
-  count.value++
+function sendChat () {
+  if (message.value) {
+    chats.value.push({
+      id,
+      time: new Date(),
+      text: message.value
+    })
+
+    message.value = ''
+  }
 }
 
 // lifecycle hooks
 onMounted(() => {
-  console.log(`The initial count is ${count.value}.`)
+  const init = [{
+    id: 0,
+    time: '01/01/2022',
+    text: 'Nothing to show. Be the first to send your message.'
+  }]
+
+  chats.value = JSON.parse(localStorage.getItem('chats')) || init
 })
 </script>
