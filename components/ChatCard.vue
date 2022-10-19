bdb945a68d77603b747882c11f27a6d30f4a4161
Hammed A. Olajide <olajide.a.hammed@gmail.com>
2022-10-19 11:27:24 +0100

    Refactoring

--- components/ChatCard.vue
+++ components/ChatCard.vue
@@ -2,7 +2,14 @@
 <template>
   <div class="flex flex-col w-full">
     <div v-if="isMe(chat.id)" class="bg-white rounded-lg bg-green-100 p-4 w-2/3 self-end">
-      {{ chat.text }}
+      <div class="pb-2">
+        {{chat.text }}
+      </div>
+
+      <div class="text-xs text-gray-500">
+        {{ new Date(chat.time).toLocaleDateString() }} at
+        {{ new Date(chat.time).toLocaleTimeString() }}
+      </div>
     </div>
 
     <div v-else class="bg-white rounded-lg bg-green-50 p-4 w-2/3">
